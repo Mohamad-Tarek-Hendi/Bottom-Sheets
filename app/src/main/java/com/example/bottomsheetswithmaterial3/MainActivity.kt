5b5543012091d942938f7df88d005a8a8ccc3f83
Mohammad Tarek Hendi <t.hindi@outlook.com>
2023-10-29 12:06:54 +0800

    * Create simple bottom sheet

--- app/src/main/java/com/example/bottomsheetswithmaterial3/MainActivity.kt
+++ app/src/main/java/com/example/bottomsheetswithmaterial3/MainActivity.kt
@@ -3,21 +3,78 @@ package com.example.bottomsheetswithmaterial3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.bottomsheetswithmaterial3.ui.theme.BottomSheetsWithMaterial3Theme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BottomSheetsWithMaterial3Theme {
 
+                val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+
+                //rememberSaveable function is used to remember and save the state of a variable across recompositions and configuration changes in Jetpack Compose.
+                var isSheetOpen by rememberSaveable {
+                    mutableStateOf(false)
+                }
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Button(
+                        onClick = {
+                            isSheetOpen = true
+                        }) {
+                        Text(text = "Click")
+                    }
+                }
+
+                if (isSheetOpen) {
+                    ModalBottomSheet(
+                        sheetState = sheetState,
+                        onDismissRequest = {
+                            isSheetOpen = false
+                        }) {
+
+                        Text(
+                            text = "Hello World!!",
+                            modifier = Modifier.fillMaxWidth(),
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 30.sp,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
 
             }
         }
